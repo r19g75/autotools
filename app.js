1054074be2bd1e530e2042a66ee0d5805042d0a4
Engineer_tools <150811867+r19g75@users.noreply.github.com>
2025-11-19 16:51:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -201,9 +201,9 @@ document.addEventListener('touchmove', e => {
 // pokaż / ukryj pole stożka
 function toggleCone() {
     const shape = document.getElementById('tankShape').value;
+    // pokaż / ukryj pole stożka
     document.getElementById('coneH').style.display = (shape === 'cylcone') ? 'block' : 'none';
 }
-
 // główny licznik
 function calcTank() {
     const D     = parseFloat(document.getElementById('tankD').value)  || 0;  // cm
@@ -233,3 +233,4 @@ function calcTank() {
          Cylinder: ${(Vcyl/1000).toFixed(2)} l` +
         (Vcone ? `<br>Stożek: ${(Vcone/1000).toFixed(2)} l` : '');
 }
+
